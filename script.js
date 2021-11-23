5dd65c380f90e6067e381f1a012e6f72129ebad3
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-23 15:28:05 +0000

    pop and comments

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const moreFruits = [...fruits, 'banana']
 
 console.log(moreFruits)
 //spread operator expands the array
+//this clones the array. doesn't work for objects inside the array - only primitive types
+
+moreFruits.pop()
+console.log(moreFruits)
