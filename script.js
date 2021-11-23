3ff0068d2009c449c7b9b76520cf315ce94d4e79
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-23 20:56:12 +0000

    slice and combine two sliced into one new array

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ console.log(moreFruits)
 
 moreFruits.pop()
 console.log(moreFruits)
+
+
+let a = [ 1, 'GS', ['dob', ['addr']]]
+console.log(a[0])
+
+const numArray = [1, 2, 3]
+numArray.slice(0, 1)
+numArray.slice(2, 4)
+const newNumArray = [
+  ...numArray.slice(0,1),
+  ...numArray.slice(2, 4)
+
+]
+console.log(newNumArray)
